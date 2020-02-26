ea9f872cc9490e2f29861a56cea8cb5626bec6f9
Puviez <boredkeeper1793@gmail.com>
2020-02-26 21:11:55 +0800

    Created Payment Controller

---
+++ controllers/paymentcontroller.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const Payment = require("../models/merchmodel.js");
+const payments = express.Router();
+
+payments.post("/", (req, res) => {
+    Payment.create(
+      {
+        owner: req.body.owner,
+        merchant: req.body.merchant,
+        description: req.body.description,
+        category: req.body.category,
+        amount: req.body.amount,
+        date: req.body.date
+      },
+      (err, createdPayment) => {
+        if (err) console.log(err.message);
+        res.json(createdPayment);
+      }
+    );
+  });
+  
+payments.get("/", (req, res) => {
+    console.log("heerrree")
+    Payment.find({}, (err, foundPayment) => {
+        res.json(foundPayment);
+    });
+});
